22cf68612fe83d0310ccf48e5cd262853102d66b
Miguel Montiel <migueel.angelom@gmail.com>
2021-12-28 12:52:32 -0600

    Creacion de carpeta para async await

---
+++ async/asyncAwait.js
@@ -0,0 +1,51 @@
+const saludo = (nombre) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Saludos usuario: " + nombre);
+            resolve(nombre)
+        }, 1500);
+    })
+}
+
+const despedida = (nombre, callback) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Hasta luego usuario: " + nombre);
+            resolve(nombre);
+        }, 1500);
+    })
+}
+
+const hablar = (nombre) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('BLA BLA BLA....');
+            resolve(nombre);
+        }, 500);
+    })
+}
+
+const conversar = (nombre, veces, callback) => {
+    if (veces > 0) {
+        hablar(() => {
+            conversar(nombre, --veces, callback)
+        })
+    } else {
+        despedida(nombre, callback);
+    }
+}
+
+console.log("Iniciando Proceso...");
+
+saludo('Miguel')
+    .then((nombre) => {
+        return hablar(nombre)
+    })
+    .then((nombre) => {
+        return despedida(nombre)
+    })
+    .then(() => {
+        console.log('Terminando Proceso...');
+    }).catch(error => {
+        console.error(new Error('Error: ' + error));
+    })
---
+++ async/callback.js
@@ -0,0 +1,58 @@
+const saludo = (nombre, callback) => {
+    setTimeout(() => {
+        console.log("Saludos usuario: " + nombre);
+        callback(null, nombre)
+    }, 1500);
+}
+
+const despedida = (nombre, callback) => {
+    setTimeout(() => {
+        console.log("Hasta luego usuario: " + nombre);
+        callback(null, nombre);
+    }, 1500);
+}
+
+const hablar = (callback) => {
+    setTimeout(() => {
+        console.log('BLA BLA BLA....');
+        callback();
+    }, 500);
+}
+
+const conversar = (nombre, veces, callback) => {
+    if (veces > 0) {
+        hablar(() => {
+            conversar(nombre, --veces, callback)
+        })
+    } else {
+        despedida(nombre, callback);
+    }
+}
+
+console.log("Iniciando Proceso...");
+
+saludo('Miguel', (error, nombre) => {
+    if(error) return new Error("Error")
+    conversar(nombre, 3, () => {
+        console.log('Proceso Terminado...');
+    })
+})
+
+
+
+
+// ******************* CALLBACK HELL ******************* //
+// saludo('Miguel', (error, nombre) => {
+//     if (error) console.log(new Error('Error: ' + error));
+//     hablar(() => {
+//         hablar(() => {
+//             hablar(() => {
+//                 despedida(nombre, () => {
+//                     if (error) console.log(new Error('Error: ' + error));
+//                     console.log('Terminando Proceso....');
+//                 });
+//             })
+//         })
+//     })
+// });
+// ******************* CALLBACK HELL ******************* //
